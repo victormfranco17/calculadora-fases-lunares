b52c4f25f71d726855b8dfcc56a7e5c6812afcb9
Victor Franco <132877861+victormfranco17@users.noreply.github.com>
2023-07-04 07:28:47 -0400

    Add files via upload

---
+++ calculadora-fases-lunares/script.js
@@ -0,0 +1,10 @@
+javascript
+$(document).ready(function() {
+  $("#calculateButton").click(function() {
+    var dateInput = $("#dateInput").val();
+
+    // Lógica para calcular las fases lunares y los días de crecimiento del cabello
+
+    $("#result").text("Resultado");
+  });
+});
